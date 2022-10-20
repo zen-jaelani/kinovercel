85d404f4d8121da0e603b4a411cc1c9c7a4ee355
Qxtlp <xi.tulop@yandex.com>
2022-10-20 12:26:34 +0700

    test

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,11 @@
-const { Router } = require('express');
-const { SuccessResponseObject } = require('../common/http');
-const demo = require('./demo.route');
+const { Router } = require("express");
+const { SuccessResponseObject } = require("../common/http");
+const demo = require("./demo.route");
 
 const r = Router();
 
-r.use('/demo', demo);
+r.use("/demo", demo);
 
-r.get('/', (req, res) => res.json(new SuccessResponseObject('express vercel boiler plate')));
+r.get("/", (req, res) => res.json("express vercel boiler plate"));
 
 module.exports = r;
