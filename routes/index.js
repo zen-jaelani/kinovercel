ed192c9102b6633bb0d8a6e7ea2d50072ac62483
Qxtlp <xi.tulop@yandex.com>
2022-10-20 20:16:07 +0700

    another

--- routes/index.js
+++ routes/index.js
@@ -2,16 +2,8 @@ const express = require("express");
 
 const Router = express.Router();
 
-const movieRoutes = require("../modules/movie/movieRoutes");
-const scheduleRoutes = require("../modules/schedule/scheduleRoutes");
-const bookingRoutes = require("../modules/booking/bookingRoutes");
-const authRoutes = require("../modules/auth/authRoutes");
 const userRoutes = require("../modules/user/userRoutes");
 
-Router.use("/movie", movieRoutes);
-Router.use("/schedule", scheduleRoutes);
-Router.use("/booking", bookingRoutes);
-Router.use("/auth", authRoutes);
 Router.use("/user", userRoutes);
 
 module.exports = Router;
