a7cf5102086172d280aeaad32c2261e7ee3b3995
Qxtlp <xi.tulop@yandex.com>
2022-10-20 14:08:43 +0700

    test

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
+const express = require('express');
 const express = require("express");
 const morgan = require("morgan");
 const cors = require("cors");
 const helmet = require("helmet");
 const xss = require("xss-clean");
 const compression = require("compression");
-const bodyParser = require("body-parser");
-const routerNavigation = require("./routes");
+const { ErrorResponseObject } = require('./common/http');
+const routes = require('./routes');
 
 const app = express();
+
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 
 app.use(morgan("dev"));
 app.use(cors());
@@ -15,13 +19,9 @@ app.options("*", cors());
 app.use(helmet());
 app.use(xss());
 app.use(compression());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-app.use(express.static("public"));
+app.use('/', routes);
 
-app.use("/", routerNavigation);
-
-
-app.all('*', (req, res) => res.status(404).json('route not defined'));
+// default catch all handler
+app.all('*', (req, res) => res.status(404).json(new ErrorResponseObject('route not defined')));
 
 module.exports = app;
