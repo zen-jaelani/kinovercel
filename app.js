9a198d6b17afaef95f7e3d6cb765b3da7b763010
Qxtlp <xi.tulop@yandex.com>
2022-10-20 12:58:45 +0700

    test2

--- app.js
+++ app.js
@@ -6,10 +6,8 @@ const xss = require("xss-clean");
 const compression = require("compression");
 const bodyParser = require("body-parser");
 const routerNavigation = require("./routes");
-require("dotenv").config();
 
 const app = express();
-const port = process.env.PORT;
 
 app.use(morgan("dev"));
 app.use(cors());
@@ -22,16 +20,8 @@ app.use(bodyParser.json());
 app.use(express.static("public"));
 
 app.use("/", routerNavigation);
-app.use("/*", (request, response) => {
-  response.status(404).send("Path not found!");
-});
 
-app.listen(port, () => {
-  console.log(`Express app is listen on port ${port} !`);
-});
 
-app.all("*", (req, res) =>
-  res.status(404).json(new ErrorResponseObject("route not defined"))
-);
+app.all('*', (req, res) => res.status(404).json('route not defined'));
 
 module.exports = app;
