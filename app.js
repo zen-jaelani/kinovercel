b77ef43ecb46865c49f1f1bb36845ce822f7111e
Qxtlp <xi.tulop@yandex.com>
2022-10-22 21:30:20 +0700

    merge index

--- app.js
+++ app.js
@@ -1,16 +1,28 @@
-const express = require('express');
-const helmet = require('helmet');
-const { ErrorResponseObject } = require('./common/http');
-const routes = require('./routes');
+const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");
+const helmet = require("helmet");
+const xss = require("xss-clean");
+const compression = require("compression");
+const bodyParser = require("body-parser");
+const { ErrorResponseObject } = require("./common/http");
+const routes = require("./routes");
 
 const app = express();
 
-app.use(express.json({ limit: '50mb' }));
-app.use(express.urlencoded({ extended: true, limit: '50mb' }));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(morgan("dev"));
+app.use(cors());
+app.options("*", cors());
 app.use(helmet());
-app.use('/', routes);
+app.use(xss());
+app.use(compression());
+app.use("/", routes);
 
 // default catch all handler
-app.all('*', (req, res) => res.status(404).json(new ErrorResponseObject('route not defined')));
+app.all("*", (req, res) =>
+  res.status(404).json(new ErrorResponseObject("route not defined"))
+);
 
 module.exports = app;
