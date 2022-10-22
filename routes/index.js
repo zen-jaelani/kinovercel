1d0ff88c2e48ff76d7fb6bc7ca80e7492ce2c755
Qxtlp <xi.tulop@yandex.com>
2022-10-22 21:08:19 +0700

    testttt

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,14 @@
-const { Router } = require('express');
-const { SuccessResponseObject } = require('../common/http');
-const demo = require('./demo.route');
+const { v4: uuidv4 } = require("uuid");
+const { Router } = require("express");
+const { SuccessResponseObject } = require("../common/http");
+const demo = require("./demo.route");
 
 const r = Router();
 
-r.use('/demo', demo);
+r.use("/demo", demo);
 
-r.get('/', (req, res) => res.json(new SuccessResponseObject('express vercel boiler plate')));
+r.get("/", (req, res) =>
+  res.json(new SuccessResponseObject(`express vercel boiler plate ${uuidv4()}`))
+);
 
 module.exports = r;
